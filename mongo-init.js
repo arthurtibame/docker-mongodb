1706611b5cb2b8f7300330b1d22d60bd32ded79e
Arthur <arthur8485@gmail.com>
2020-07-12 21:50:15 +0800

    first commit

---
+++ mongo-init.js
@@ -0,0 +1,64 @@
+db.createUser(
+    {
+        user: "teammate1",
+        pwd: "eb102",
+        roles: [
+            {
+                role: "readWrite",
+                db: "<<db name>>"
+            }
+        ]
+    }
+);
+
+db.createUser(
+    {
+        user: "teammate2",
+        pwd: "eb102",
+        roles: [
+            {
+                role: "readWrite",
+                db: "<<db name>>"
+            }
+        ]
+    }
+);
+
+db.createUser(
+    {
+        user: "teammate3",
+        pwd: "eb102",
+        roles: [
+            {
+                role: "readWrite",
+                db: "<<db name>>"
+            }
+        ]
+    }
+);
+
+db.createUser(
+    {
+        user: "teammate4",
+        pwd: "eb102",
+        roles: [
+            {
+                role: "readWrite",
+                db: "<<db name>>"
+            }
+        ]
+    }
+);
+
+db.createUser(
+    {
+        user: "teammate5",
+        pwd: "eb102",
+        roles: [
+            {
+                role: "readWrite",
+                db: "<<db name>>"
+            }
+        ]
+    }
+);
